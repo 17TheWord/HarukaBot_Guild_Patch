58ea3c28a8f5b0af7fcb8b181d347ffe19362b24
17TheWord <54731914+17TheWord@users.noreply.github.com>
2022-06-09 01:31:09 +0800

    Update delete_sub.py

--- plugins/haruka_bot/plugins/sub/delete_sub.py
+++ plugins/haruka_bot/plugins/sub/delete_sub.py
@@ -1,36 +1,51 @@
 from nonebot import on_command
+from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.typing import T_State
 from plugins.nonebot_plugin_guild_patch import GuildMessageEvent
-
 from ...GuildSuperUsers import GuildSuperUserList
+
 from ...database import DB as db
-from ...utils import to_me
+from ...utils import get_type_id, permission_check, to_me, handle_uid
 
-add_guild_admin_sub = on_command("添加管理", aliases={"添加"}, rule=to_me(), priority=5)
-add_guild_admin_sub.__doc__ = """添加管理 ID"""
+delete_sub = on_command("取关", aliases={"删除主播"}, rule=to_me(), priority=5)
+delete_sub.__doc__ = """取关 UID"""
+
+delete_sub.handle()(permission_check)
+
+delete_sub.handle()(handle_uid)
 
 
-@add_guild_admin_sub.handle()
-async def _(event: GuildMessageEvent):
-    if str(event.user_id) in list(GuildSuperUserList):
-        if "at" in str(event.raw_message):
-            guild_user_id = str(event.raw_message)[str(event.raw_message).index(']') - 18: str(event.raw_message).index(']')]
-            await add_guild_admin_uid(guild_admin_uid=guild_user_id)
+@delete_sub.got("uid", prompt="请输入要取关的UID")
+async def _(event: MessageEvent, state: T_State):
+    if isinstance(event, GuildMessageEvent):
+        subs = await db.get_guild_admin_sub_list()
+        guild_admin_uid_list = []
+        for guild_admin in subs:
+            guild_admin_uid_list.append(guild_admin.guild_admin_uid)
+
+        if str(event.user_id) in list(GuildSuperUserList):
+            await del_uin(event=event, state=state)
+
+        elif str(event.user_id) in guild_admin_uid_list:
+            await del_uin(event=event, state=state)
+
         else:
-            await add_guild_admin_sub.finish("请正确使用命令： 添加 @xxx")
-
+            await delete_sub.finish("您无权限进行此操作")
     else:
-        await add_guild_admin_sub.finish("您无权限进行此操作")
+        await del_uin(event=event, state=state)
 
 
-async def add_guild_admin_uid(guild_admin_uid):
-    """根据 ID添加管理员"""
-    guild_admin_list = await db.get_guild_admin_sub_list()
+async def del_uin(event, state):
+    """根据 UID 删除 UP 主订阅"""
+    uid = state["uid"]
+    name = getattr(await db.get_user(uid=uid), "name", None)
+    if name:
+        result = await db.delete_sub(
+            uid=uid, type=event.message_type, type_id=get_type_id(event)
+        )
+    else:
+        result = False
 
-    if guild_admin_uid in guild_admin_list:
-        return add_guild_admin_sub.finish(f"（{guild_admin_uid}）已经是管理了")
-
-    result = await db.add_guild_admin_sub(guild_admin_uid=guild_admin_uid)
     if result:
-        return await add_guild_admin_sub.finish(f"已添加管理（{guild_admin_uid}）")
-    else:
-        return await add_guild_admin_sub.finish("添加失败")
+        await delete_sub.finish(f"已取关 {name}（{uid}）")
+    await delete_sub.finish(f"UID（{uid}）未关注")
