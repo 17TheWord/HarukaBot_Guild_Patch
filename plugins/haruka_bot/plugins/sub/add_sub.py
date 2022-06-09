648c01884ed542840169d1404498e98556c4a8f1
17TheWord <54731914+17TheWord@users.noreply.github.com>
2022-06-09 13:37:09 +0800

    Update add_sub.py

--- plugins/haruka_bot/plugins/sub/add_sub.py
+++ plugins/haruka_bot/plugins/sub/add_sub.py
@@ -1,6 +1,7 @@
 from bilireq.exceptions import ResponseCodeError
 from bilireq.user import get_user_info
 from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Bot
 from nonebot.adapters.onebot.v11.event import MessageEvent
 from nonebot.typing import T_State
 from plugins.nonebot_plugin_guild_patch import GuildMessageEvent
@@ -18,23 +19,55 @@ add_sub.handle()(handle_uid)
 
 
 @add_sub.got("uid", prompt="请输入要关注的UID")
-async def _(event: MessageEvent, state: T_State):
+async def _(bot: Bot, event: MessageEvent, state: T_State):
     if isinstance(event, GuildMessageEvent):
         subs = await db.get_guild_admin_sub_list()
+
+        # 初始化数据库中管理员ID列表
         guild_admin_uid_list = []
+
+        # 数据库中的用户列表
         for guild_admin in subs:
             guild_admin_uid_list.append(guild_admin.guild_admin_uid)
 
+        # 判断是否为超管
         if str(event.user_id) in list(GuildSuperUserList):
             await add_uid(event=event, state=state)
 
+        # 判断是否在数据库表中
         elif str(event.user_id) in guild_admin_uid_list:
+            await add_uid(event=event, state=state)
+
+        # 判断是否为管理员身份组
+        elif if_admin_group(bot=bot, event=event):
             await add_uid(event=event, state=state)
 
         else:
             await add_sub.finish("您无权限进行此操作")
+
     else:
         await add_uid(event=event, state=state)
+
+
+async def if_admin_group(bot, event):
+    # 获取用户信息
+    guild_user_info = await bot.call_api("get_guild_member_profile", guild_id=event.guild_id, user_id=event.user_id)
+
+    # 获取 ENV 中频道管理员身份组名称
+    guild_admin_name_list = list(bot.config.haruka_guild_admin_group_name)
+
+    # 初始化用户所在的身份组列表
+    guild_user_roles_list = []
+
+    # 用户频道所在身份组的列表
+    for role in guild_user_info['roles']:
+        guild_user_roles_list.append(role['role_name'])
+
+    # 判断用户所属的身份组中是否包含 ENV 中的身份组
+    for guild_admin_name in guild_admin_name_list:
+        if guild_admin_name in guild_user_roles_list:
+            return True
+    return False
 
 
 async def add_uid(event, state):
